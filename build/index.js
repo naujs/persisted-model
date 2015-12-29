f06e3c0e34597a78bafced4aefd6a90712e970e2
Tan Nguyen <tan@sharkpunch.com>
2015-12-28 20:04:36 -0600

    Update deps

--- build/index.js
+++ build/index.js
@@ -36,6 +36,13 @@ var PersistedModel = (function (_Model) {
     value: function name() {
       throw 'Must implement';
     }
+  }, {
+    key: 'onAfterFind',
+    value: function onAfterFind() {
+      var options = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+
+      return this;
+    }
   }]);
 
   return PersistedModel;
